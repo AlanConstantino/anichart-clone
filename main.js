509b0266bc0bb41cb8ca32c057ddbeabdf9aaeda
AlanConstantino <constantinoalan98@gmail.com>
2020-06-17 21:57:15 -0700

    TEST 2

--- main.js
+++ main.js
@@ -84,48 +84,48 @@ const variables = {
 };
 const checkBox = document.getElementById('nsfw');
 
-// sets the current year to each navbar item
-// i.e. "Winter 2020", "Spring 2020", etc.
+// // sets the current year to each navbar item
+// // i.e. "Winter 2020", "Spring 2020", etc.
 Util.setSeasonYearNavbar(currentYear);
 
-// creating instances of the Api and Dom class
-const api = new Api(url, options, variables, query);
-const dom = new Dom();
+// // creating instances of the Api and Dom class
+// const api = new Api(url, options, variables, query);
+// const dom = new Dom();
 
-// shows media of the season we are currently in
-const request = api.makeRequest(query, currentSeason);
-request.then(data => api.handleData(data));
+// // shows media of the season we are currently in
+// const request = api.makeRequest(query, currentSeason);
+// request.then(data => api.handleData(data));
 
-// will request media based on what navitem you clicked
-// will also show nsfw media if checkbox has been clicked
-// i.e. If you clicked on Winter, will show you Winter media
-function requestNavbarSeasonMedia(navitem) {
-  navitem.addEventListener('click', () => {
-    Dom.clearMainTag(); // clears everything within the <main> tag
-    navbarSeason = navitem.id.toUpperCase();
-    const request = api.makeRequest(query, navbarSeason);
-    if (checkBox.checked) {
-      request.then(data => api.handleData(data, true));
-      return;
-    }
-    request.then(data => api.handleData(data, false));
-  });
-}
+// // will request media based on what navitem you clicked
+// // will also show nsfw media if checkbox has been clicked
+// // i.e. If you clicked on Winter, will show you Winter media
+// function requestNavbarSeasonMedia(navitem) {
+//   navitem.addEventListener('click', () => {
+//     Dom.clearMainTag(); // clears everything within the <main> tag
+//     navbarSeason = navitem.id.toUpperCase();
+//     const request = api.makeRequest(query, navbarSeason);
+//     if (checkBox.checked) {
+//       request.then(data => api.handleData(data, true));
+//       return;
+//     }
+//     request.then(data => api.handleData(data, false));
+//   });
+// }
 
-// checkbox event handler function
-function checkBoxEventHandler() {
-  Dom.clearMainTag();
-  console.log(navbarSeason);
-  if (navbarSeason === '') {
-    navbarSeason = currentSeason;
-  }
-  const request = api.makeRequest(query, navbarSeason);
-  request.then(data => api.handleData(data, checkBox.checked));
-}
+// // checkbox event handler function
+// function checkBoxEventHandler() {
+//   Dom.clearMainTag();
+//   console.log(navbarSeason);
+//   if (navbarSeason === '') {
+//     navbarSeason = currentSeason;
+//   }
+//   const request = api.makeRequest(query, navbarSeason);
+//   request.then(data => api.handleData(data, checkBox.checked));
+// }
 
-// clicking on a navitem changes media to that season
-const navbar = [...dom.navbar];
-navbar.forEach(navitem => requestNavbarSeasonMedia(navitem));
+// // clicking on a navitem changes media to that season
+// const navbar = [...dom.navbar];
+// navbar.forEach(navitem => requestNavbarSeasonMedia(navitem));
 
-// whenever you click on the nsfw checkbox, checkBoxValidation() gets fired off
-checkBox.addEventListener('change', checkBoxEventHandler);
+// // whenever you click on the nsfw checkbox, checkBoxValidation() gets fired off
+// checkBox.addEventListener('change', checkBoxEventHandler);
