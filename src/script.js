f1b70cc677938d527c92f0f3b04bac53f28a316d
AlanConstantino <constantinoalan98@gmail.com>
2020-06-30 03:02:40 -0700

    Added load icon functionality

--- src/script.js
+++ src/script.js
@@ -15,78 +15,80 @@ const options = {
   body: {}
 };
 const query = `
-  query ($id: Int, $page: Int, $perPage: Int, $search: String, $season: MediaSeason, $year: Int) {
-      Page(page: $page, perPage: $perPage) {
-        pageInfo {
-          total
-          currentPage
-          lastPage
-          hasNextPage
-          perPage
-        }
-        media(id: $id, search: $search, season: $season, seasonYear: $year) {
-          id
-          siteUrl
-          rankings {
-            rank
+    query ($id: Int, $page: Int, $perPage: Int, $search: String, $season: MediaSeason, $year: Int) {
+        Page(page: $page, perPage: $perPage) {
+          pageInfo {
+            total
+            currentPage
+            lastPage
+            hasNextPage
+            perPage
           }
-          isAdult
-          startDate {
-            year
-            month
-            day
-          }
-          source
-          status
-          episodes
-          description(asHtml: true)
-          chapters
-          volumes
-          studios(sort: NAME) {
-            edges {
-              isMain
-              node {
-                name
+          media(id: $id, search: $search, season: $season, seasonYear: $year) {
+            id
+            siteUrl
+            rankings {
+              rank
+            }
+            isAdult
+            startDate {
+              year
+              month
+              day
+            }
+            source
+            status
+            episodes
+            description(asHtml: true)
+            chapters
+            volumes
+            studios(sort: NAME) {
+              edges {
+                isMain
+                node {
+                  name
+                }
               }
             }
-          }
-          coverImage {
-            extraLarge
-            large
-            medium
-            color
-          }
-          airingSchedule {
-            nodes {
-              episode
-              airingAt
-              timeUntilAiring
+            coverImage {
+              extraLarge
+              large
+              medium
+              color
             }
-          }
-          averageScore
-          genres
-          season
-          seasonYear
-          type
-          format
-          popularity
-          title {
-            english
-            romaji
+            airingSchedule {
+              nodes {
+                episode
+                airingAt
+                timeUntilAiring
+              }
+            }
+            averageScore
+            genres
+            season
+            seasonYear
+            type
+            format
+            popularity
+            title {
+              english
+              romaji
+            }
           }
         }
       }
-    }
-  `;
+    `;
 const variables = {
   year: currentYear,
   page: 1
 };
 const checkBox = document.getElementById('nsfw');
+const loadIcon = document.getElementsByClassName('loading-icon')[0];
 
 // creating instances of the Api and Dom class
 const api = new Api(url, options, variables, query);
 const dom = new Dom();
+
 
 // will request media based on what navitem you clicked
 // will also show nsfw media if checkbox has been clicked
@@ -94,21 +96,28 @@ const dom = new Dom();
 function requestNavbarSeasonMedia(navitem) {
   navitem.addEventListener('click', () => {
     Dom.clearMainTag(); // clears everything within the <main> tag
+    displayLoadIcon();
     navbarSeason = navitem.id.toUpperCase();
     const request = api.makeRequest(query, navbarSeason);
     if (checkBox.checked) {
       request.then(data => api.handleData(data, true));
-      return;
+    } else {
+      request.then(data => api.handleData(data, false));
     }
-    request.then(data => api.handleData(data, false));
+    removeLoadIcon();
   });
 }
 
 // checkbox event handler function
 function checkBoxEventHandler() {
   Dom.clearMainTag();
+  displayLoadIcon();
   const request = api.makeRequest(query, navbarSeason);
-  request.then(data => api.handleData(data, checkBox.checked));
+  request.then(data => {
+    api.handleData(data, checkBox.checked);
+    // removeLoadIcon();
+  });
+  removeLoadIcon();
 }
 
 // sets the current year to each navbar item
@@ -117,7 +126,9 @@ Util.setSeasonYearNavbar(currentYear);
 
 // shows media of the season we are currently in
 const request = api.makeRequest(query, currentSeason);
-request.then(data => api.handleData(data));
+request.then(data => {
+  api.handleData(data);
+});
 
 // clicking on a navitem changes media to that season
 const navbar = [...dom.navbar];
@@ -125,3 +136,31 @@ navbar.forEach(navitem => requestNavbarSeasonMedia(navitem));
 
 // whenever you click on the nsfw checkbox, checkBoxValidation() gets fired off
 checkBox.addEventListener('change', checkBoxEventHandler);
+
+// checks the ready state of the document and shows/removes loading icon
+document.addEventListener('readystatechange', () => {
+  const state = document.readyState;
+  switch (state) {
+    case 'complete':
+      removeLoadIcon();
+      break;
+    case 'loading':
+    case 'interactive':
+    default:
+      displayLoadIcon();
+  }
+});
+
+// when the window loads completely, remove the load icon
+window.addEventListener('load', removeLoadIcon);
+
+// rewrite without setTimeout so load icon leaves once everything is loaded
+function removeLoadIcon() {
+  setTimeout(() => {
+    loadIcon.style.display = 'none';
+  }, 300);
+}
+
+function displayLoadIcon() {
+  loadIcon.style.display = 'block';
+}
